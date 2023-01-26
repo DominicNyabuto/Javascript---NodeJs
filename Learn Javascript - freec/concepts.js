9d80954cf5edc4db7f6b2d738235577bd5bf82ed
Dominic Nyabuto <dominicnyabuto@outlook.com>
2023-01-26 03:53:14 +0300

    Add files via upload
    JS basics

---
+++ Learn Javascript - freec/concepts.js
@@ -0,0 +1,46 @@
+var number = 5 // in-line comment 
+
+/* this is harder than I thought, or maybe I 
+am lazier than I thought 
+*/ 
+
+var myName = "Beau"
+
+myName = 8 
+
+let ourName ="dominicNyabuto"
+
+const pi = 3.141592653589793 //can never change 
+
+var a; // ; is not required but is used to show end of the line 
+
+var b = 2; // b is assigned to b 
+
+a = 7; //takes variable a and assigns value 2 to it 
+b = a; 
+console.log (a) // like print in in python 
+
+//capitalization matters in JavaScript 
+
+//Declarations 
+
+var StudyToday; // not same as studytoday 
+var keepSchedule; //not same as keepschedule 
+var writeCamelCase; //not same as WriteCamelcase or any variatiions
+
+//math
+
+var sum = 24 + 20; 
+var sub = 23 - 19; 
+
+// incrementing a number 
+var myVar = 78; 
+
+myVar = myVar + 1; 
+
+//also written as 
+myVar++; 
+
+//decrenting 
+myVar--; //or 
+myVar = myVar - 1; 
