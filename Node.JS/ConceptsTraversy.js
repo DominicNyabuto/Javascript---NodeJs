f92a6834ca3b224a5f6777170c4f1d6f3f79e522
Dominic Nyabuto <dominicnyabuto@outlook.com>
2023-01-29 00:03:04 +0300

    Add files via upload

---
+++ Node.JS/ConceptsTraversy.js
@@ -0,0 +1,23 @@
+/*-Node.js is just a run time environment
+  version of JavaScript - meaning that it 
+  runs on your machine rather than a 
+  browser like traditional JS, that is why
+  VSCode shows NodeJS instead of JS, it 
+  is not a language or framework
+  - Servside JS frameworks are like Express, 
+  Next.js
+
+  -Node.JS is written in C++ and is helps run 
+  JS on servers.  
+  - Preliqusites: HTTP, JSON, Arrow Functions, 
+  Promises, MVC Pattern
+  - Benefits; fast, efficient, scallable, 
+  popular, front and back end usage, uses
+  non-blocking I/O (works on a single thread, 
+    supports thousands of concurrent connections, 
+    optimized for scalability with many I/O, fast
+    and efficient)
+  -Best Projects for Node.JS: REST API & microservices, 
+  Real Time Services (chat, live updates), CRUD APps, 
+  tools & utilities)- non CPU intensive tasks
+*/
