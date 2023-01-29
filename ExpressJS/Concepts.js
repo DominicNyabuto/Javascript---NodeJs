670a6825adec4878ba75a22a451faf1b93e4ac6e
Dominic Nyabuto <dominicnyabuto@outlook.com>
2023-01-29 15:27:24 +0300

    Add files via upload
    basics of Express JS

---
+++ ExpressJS/Concepts.js
@@ -0,0 +1,30 @@
+/*
+ - Express is a web framework for NodeJS . It is server-side
+ as opposed to React, Angular, & Vue  which are for the 
+ client-side (front end). Use Express and React for full stack 
+ applications. 
+ - It is very basic compared to Django for Python, not for high level 
+ stuff
+- Minimalist but give syou full control of what to create 
+
+Benefits: 
+- easier for web apps than plani Node.JS 
+- used for both server rendered apps and API.Microservices 
+- very light and fast 
+- full contoll 
+- most populat NOde framework 
+- better when mixing front end and back end with JS 
+
+Basic Route handling: 
+- is simple to handle requests and routes 
+- can parse incoming data with the Body Parse 
+- Used with MongoDB, Postgres, MySQL
+
+Express Middleware 
+- MIDDLEWARE FUNCTIONS - are functions that have acces to the request 
+and response objects. They are Express built in functions but you can 
+get more middleware functions from 3rd party packages and custom 
+middleware. 
+- They help execute any code, make changes to the request.response 
+objects, end response cycle, and call next middleware in the stack 
+*/
